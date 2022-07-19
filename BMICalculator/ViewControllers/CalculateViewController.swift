9b4ae992a3e2908badbf8ffe9527bb595955bc90
Aleksandr F <a7214370@gmail.com>
2022-07-19 22:45:26 +0300

    Setup project

--- BMICalculator/ViewControllers/CalculateViewController.swift
+++ BMICalculator/ViewControllers/CalculateViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculateViewController.swift
 //  BMICalculator
 //
 //  Created by Aleksandr F. on 18.07.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
